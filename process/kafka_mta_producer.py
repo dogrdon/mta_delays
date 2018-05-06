8b079c8f45eaca2cbebeff3adcc603095b3d3367
Drew Gordon <drew.s.gordon@gmail.com>
2018-05-06 05:41:31 +0000

    add loose processing
    and promises of additional work

--- process/kafka_mta_producer.py
+++ process/kafka_mta_producer.py
@@ -14,7 +14,19 @@ from bson.json_util import dumps
 MTA_DELAYS_IN_KAFKA_TOPIC = 'mta-delays'
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', 
-					     value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+					     value_serializer=lambda v: json.dumps(v).encode('utf-8') if isinstance(v, dict) else v.encode('utf-8'),
+					     acks="all")
+
+
+
+
+
+
+def clean_id(jsonrecord):
+	jsonrecord['oid'] = jsonrecord['_id']['$oid']
+	del jsonrecord['_id']
+	return jsonrecord
+
 
 def get_raw_delays_and_send_to_kafka():
 	new_last_record_timestamp = None
@@ -26,23 +38,35 @@ def get_raw_delays_and_send_to_kafka():
 	if last_record == []:
 		# get all the records
 		records = list(mta_delays_client.get_all_records())
+		
 
 	else:
 		# only get records since last timestamp
 		last_record_timestamp = last_record[0]['last_timestamp_unix']
 		records = list(mta_delays_client.get_records_gt('timestamp_unix', last_record_timestamp))
 
+	print("Found {} new records to send.".format(str(len(records))))
 
+	sent_records = 0
 	for record in records:
 		# send record to kafka topic
 		try:
 			serialized_record = dumps(record)
-			producer.send(MTA_DELAYS_IN_KAFKA_TOPIC, serialized_record)
+			json_record = clean_id(json.loads(serialized_record))
+			prepared_record = json.dumps(json_record)
+			print("Sending {} ".format(prepared_record))
+			obj_key = str(record['_id'])
+			#print("setting key to: {}".format(obj_key))
+			# Sending messages might fail bc producer timesout too quickly, set a timeout to resolve: https://github.com/dpkp/kafka-python/issues/563
+			res = producer.send(MTA_DELAYS_IN_KAFKA_TOPIC, key=obj_key.encode(), value=prepared_record).get(timeout=30)
 			new_last_record_timestamp = record['timestamp_unix']
+			
+			sent_records += 1
 			
 		except Exception as e:
 			print("Tried sending {} from {} to topic and it failed: {}, Quiting for now, will start from where left off".format(record['_id'], record['timestamp'], e))
 			break
+
 
 	if new_last_record_timestamp is not None:
 		update_last_batch = {}
@@ -51,6 +75,7 @@ def get_raw_delays_and_send_to_kafka():
 		print('saving last_batch_run: {}'.format(new_last_record_timestamp))
 		last_batch_store_client.save_record(update_last_batch)
 
+	print("Sent {} new records to {} topic.".format(sent_records, MTA_DELAYS_IN_KAFKA_TOPIC))
 
 
 if __name__ == '__main__':
--- process/mta_processor.py
+++ process/mta_processor.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from connection import MongoConn
 import pyspark
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
@@ -16,22 +19,33 @@ sc = SparkContext()
 sc.setLogLevel("ERROR")
 spark = SparkSession(sc)
 
+def raw_text_processor():
+
+    pass
+
+
+def get_nearest_weather_obs(uts):
+    '''
+        Takes a unix timestamp and fetches the weather record
+        closes in time to that and returns that record
+    '''
+
+    pass
+# UDF....
+
 
 df = spark.readStream.format("kafka") \
 	.option("kafka.bootstrap.servers","localhost:9092") \
 	.option("subscribe", "mta-delays") \
 	.option("startingOffsets", "earliest").load()
 
-
 jsonschema = StructType().add("timestamp", StringType()) \
                          .add("timestamp_unix", IntegerType()) \
-                         .add("_id", StructType() \
-                            .add("$oid", StringType()) \
+                         .add("oid", StringType()) \
                          .add("lines", ArrayType(StructType() \
                             .add("line", StringType()) \
                             .add("status", StringType()) \
-                            .add("raw_text", StringType()))))
-
+                            .add("raw_text", StringType())))
 
 mta_stream = df.select(from_json(col("value") \
                                 .cast("string"), jsonschema) \
@@ -39,7 +53,19 @@ mta_stream = df.select(from_json(col("value") \
 
 mta_data = mta_stream.select("parsed_mta_values.*")
 
-print(mta_stream.printSchema)
+mta_data_deduped = mta_data.dropDuplicates(['timestamp_unix'])
 
-qry = mta_data.writeStream.outputMode("append").format("console").start()
+exploded_df = mta_data_deduped.select("*", explode("lines").alias("exploded_lines"))
+
+split_df = exploded_df.select('timestamp', 'timestamp_unix', 'oid', 'exploded_lines.*')
+
+# TODO: JOIN ON A WEATHER RECORD
+
+# TODO: DEAL WITH RAW TEXT (opt for now)
+
+# WRITE TO A KAFKA OUT STREAM FOR PICKUP
+
+qry = split_df.writeStream.outputMode("append").format("console").start()
 qry.awaitTermination()
+
+
